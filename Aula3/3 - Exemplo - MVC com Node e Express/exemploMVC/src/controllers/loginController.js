3ef97706e420f49c99b6acf466d9e57f1a94cdde
luizriato <142940208+luizriato@users.noreply.github.com>
2024-08-27 21:56:16 -0300

    update

--- Aula3/3 - Exemplo - MVC com Node e Express/exemploMVC/src/controllers/loginController.js
+++ Aula3/3 - Exemplo - MVC com Node e Express/exemploMVC/src/controllers/loginController.js
@@ -1,10 +1,28 @@
 const loginModel = require('../models/loginModel')
 const login = new loginModel()
 const logged = login.isLogged()
-
+/*
+#swagger.tags = ['login']
+#swagger.summary = 'Call some function'
+#swagger.description = 'This endpoint will show if user is logged'
+*/
 exports.getLogin = ((req, resp) => {    
     resp.send(`<h1>${logged}</h1>`)
 })
 exports.getIsLogged = ((req, res) => {    
     res.render("loginView", { logged: logged })
 })
+
+exports.createUser = (req, res) => {
+    //#swagger.tags = ['Create user']
+    const { email, password } = req.body;
+    // Assuming some user creation logic here
+    res.status(200).json({ message: "User added successfully" });
+};
+
+exports.getbyId = (req, res) => {
+    const filter = req.query.filter;
+    const version = req.header('version');
+    // Assuming some user retrieval logic here
+    return res.status(200).send("Getting User");
+};
--- Aula3/3 - Exemplo - MVC com Node e Express/exemploMVC/src/routes/loginRoute.js
+++ Aula3/3 - Exemplo - MVC com Node e Express/exemploMVC/src/routes/loginRoute.js
@@ -5,4 +5,6 @@ const loginController = require('../controllers/loginController')
 
 router.get("/login", loginController.getLogin)
 router.get('/logged', loginController.getIsLogged)
+router.post("/create", loginController.createUser);
+router.post('/user/:id', loginController.getbyId);
 module.exports = router
