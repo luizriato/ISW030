29d8a53c49fc78480ea03b259915ebd8b20f3336
luizriato <142940208+luizriato@users.noreply.github.com>
2024-09-03 20:37:36 -0300

    update

--- Exercicio-parte1/exemploMVC/index.js
+++ Exercicio-parte1/exemploMVC/index.js
@@ -10,7 +10,9 @@ app.use(temperatureRoute)
 
 app.set('view engine', 'ejs')
 app.set('views', './src/views/')
-
+app.get(('/',(req,res)=>{
+    res.render('header')
+}))
 app.listen(3000, function(){
     console.log('App rodando na porta 3000')
 })
