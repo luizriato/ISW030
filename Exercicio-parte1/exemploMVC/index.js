8ab20647865c8390b9f9923568548b92d3296ecd
luizriato <142940208+luizriato@users.noreply.github.com>
2024-09-03 20:29:55 -0300

    update

---
+++ Exercicio-parte1/exemploMVC/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const loginRoute = require('./src/routes/loginRoute')
+const temperatureRoute = require('./src/routes/temperatureRoute')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(loginRoute)
+app.use(temperatureRoute)
+
+app.set('view engine', 'ejs')
+app.set('views', './src/views/')
+
+app.listen(3000, function(){
+    console.log('App rodando na porta 3000')
+})
--- Exercicio-parte1/exemploMVC/src/routes/loginRoute.js
+++ Exercicio-parte1/exemploMVC/src/routes/loginRoute.js
@@ -5,6 +5,4 @@ const loginController = require('../controllers/loginController')
 
 router.get("/login", loginController.getLogin)
 router.get('/logged', loginController.getIsLogged)
-router.post("/create", loginController.createUser);
-router.post('/user/:id', loginController.getbyId);
 module.exports = router
